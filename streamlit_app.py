b38793eba041ea85b077b82da398c4ada8f7fed0
ahvsrinivas <ahvs@hotmail.com>
2023-02-24 14:44:56 -0500

    Update streamlit_app.py
    sdfs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
-print("first file")
-streamlit.title("hai")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
