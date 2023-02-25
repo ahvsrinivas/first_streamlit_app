b787156afca8cd7fb1c11653a7908209d828777d
ahvsrinivas <ahvs@hotmail.com>
2023-02-25 01:03:20 -0500

    Update streamlit_app.py
    sdfs

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice1 = streamlit.text_input1('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice1)
-fruityvice_response1 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice1)
-#streamlit.text(fruityvice_response)
-fruityvice_normalized1 = pandas.json_normalize(fruityvice_response1.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized1)
+fruit_choice1 = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice1)
+#fruityvice_response1 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice1)
+##streamlit.text(fruityvice_response)
+#fruityvice_normalized1 = pandas.json_normalize(fruityvice_response1.json())
+## write your own comment - what does this do?
+#streamlit.dataframe(fruityvice_normalized1)
