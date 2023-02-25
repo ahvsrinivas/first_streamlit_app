91037ab0b98074dae3b23c3da06497b08023f9b9
ahvsrinivas <ahvs@hotmail.com>
2023-02-25 00:56:02 -0500

    Update streamlit_app.py
    sdfs

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("Fruit laod list contains")
 #streamlit.text(my_data_row)
 streamlit.dataframe(my_data_row)
